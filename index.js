3e8ac4d16a0454904bc1cae14cde95a248f1949f
paulinad98 <paulina.drop@student.put.poznan.pl>
2021-10-09 13:13:10 +0200

    delete files

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-// Tasks: 
+// Tasks:
 
 // - write the JS so that the word count appears on the screen
 // - update the count as the user types
+
+const textArea = document.getElementById("txtid");
+
+textArea.addEventListener("txtid", () => {
+  console.log(textArea.value.split(" "));
+});
